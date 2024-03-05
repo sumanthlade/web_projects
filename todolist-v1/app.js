196a6c296d0e822fa16f83b59374df0cf07e1ca1
Sumanth Lade <ladesaisumanth2000@gmail.com>
2024-03-05 16:36:49 -0600

    Added to-do list v1 web app

---
+++ todolist-v1/app.js
@@ -0,0 +1,24 @@
+const express=require('express')
+const parser=require('body-parser')
+const date=require(__dirname+'/date.js')
+
+const app=express()
+
+app.set('view engine','ejs')
+app.use(parser.urlencoded({extended:true}))
+app.use(express.static("public"))
+app.get("/about",(req,res)=>{
+  res.render('about')
+})
+var listArray=[]
+app.get("/",(req,res)=>{
+  let day=date.getDay()
+  res.render('list',{kindOfDay:day,listItem:listArray})
+})
+app.post("/",(req,res)=>{
+  var newItem=req.body.newItem;
+  if(newItem!="")
+  listArray.push(newItem)
+  res.redirect("/");
+})
+app.listen(process.env.port||3000,()=>{})
